13d5ef8a4b2e4522ee11d95e5638686840ebfe9f
Martin Bieker <martin.bieker@udo.edu>
2021-01-21 23:41:38 +0100

    Add scaffolding for Procurment Process

---
+++ procuremonkey/procuremonkey/doctype/procurement_process/procurement_process.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Martin Bieker and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Procurement Process', {
+	// refresh: function(frm) {
+
+	// }
+});
